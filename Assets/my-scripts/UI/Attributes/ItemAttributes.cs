773beb948cce24423b54f7ec4ea44b393bd298bf
furkankeep <furkan.tokkan@keepgames.co>
2024-02-03 02:31:27 +0300

    attr

--- Assets/my-scripts/UI/Attributes/ItemAttributes.cs
+++ Assets/my-scripts/UI/Attributes/ItemAttributes.cs
@@ -2,20 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class ItemAttributes : MonoBehaviour
 {
     public int Strength = 0;
     public int Dexterity = 0;
     public int Intelligence = 0;
-
-    private void Awake()
-    {
-        if (Strength == 0 && !Application.isPlaying)
-        {
-            Strength = UnityEngine.Random.Range(1, 10);
-            Dexterity = UnityEngine.Random.Range(1, 10);
-            Intelligence = UnityEngine.Random.Range(1, 10);
-        }
-    }
 }
